a6fcf6401b59f4221055f99b680da2b998093f52
Alexander Semion <spin7ion@gmail.com>
2016-02-01 17:06:53 +0300

    strange cost function

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 ### Clear workspace
-rm(list = ls())
+rm(list = ls()) 
 ### Load utility sources
 source("load_data.R") #loader helper
 source("showDataImg.R") #helper function to show dataimg with label
@@ -35,12 +35,29 @@ Y <- as.matrix(trainLabels)
 ###Divide by 2 classes
 xzero <- X[Y==0,]
 xone  <-X[Y==1,]
+xtwo  <-X[Y==2,]
+xthree  <-X[Y==3,]
+xfour  <-X[Y==4,]
+xfive  <-X[Y==5,]
+xsix  <-X[Y==6,]
+xseven  <-X[Y==7,]
+xeight  <-X[Y==8,]
+xnine  <-X[Y==9,]
+
 yzero <- Y[Y==0]
 yone <- Y[Y==1]
+ytwo <- Y[Y==2]
+ythree <- Y[Y==3]
+yfour <- Y[Y==4]
+yfive <- Y[Y==5]
+ysix <- Y[Y==6]
+yseven <- Y[Y==7]
+yeight <- Y[Y==8]
+ynine <- Y[Y==9]
 
 #We will now concatenate both matrices so we can feed them into our classifier
-xbinary <- rbind(xzero, xone)
-ybinary <- c(yzero,yone)
+xbinary <- rbind(xzero, xnine)
+ybinary <- c(yzero,ynine)
 
 #Add ones to our X to vectorize and act as intercept terms
 X1 <- cbind(rep(1,nrow(xbinary)),xbinary)#1 is inserted because of theta0
@@ -48,14 +65,17 @@ X1Size <- nrow(X1)
 
 ### moreThanZero
 moreThanZero <- function(z){
-  return(min(max(0,sign(z)),1))
+  return(pmax(0,z))
+}
+dMoreThanZero <- function(z){
+  return(pmin(pmax(0,sign(z)),1))
 }
 
 ### Create cost function
 JwithReg <- function(theta)
 {
   hyperplane<-X1%*%theta
-  J <- -(1/X1Size)*sum( ybinary*moreThanZero(1-hyperplane)+(1-ybinary)*(hyperplane+1) )
+  J <- (1/X1Size)*sum( ybinary*moreThanZero(1-hyperplane)+(1-ybinary)*moreThanZero(hyperplane+1) )
   return(J)
 }
 
@@ -65,14 +85,18 @@ JwithReg <- function(theta)
 dJwithReg <- function(theta)
 {
   hyperplane<-X1%*%theta
-  dJ <- -(1/X1Size)*sum( -X1*ybinary*moreThanZero(1-hyperplane)+(1-ybinary)*X1*moreThanZero(1+hyperplane) )
+  dJ <- (1/X1Size)*colSums( -X1*ybinary*dMoreThanZero(1-hyperplane)+(1-ybinary)*X1*dMoreThanZero(1+hyperplane) )
   return(dJ)
 }
 
 grad.descent <- function(theta, maxit){
   alpha = .05 # set learning rate
+  maxit = 100
   for (i in 1:maxit) {
+    cost_theta <- JwithReg(theta)
+    cat("Cost theta: ", cost_theta)
     theta <- theta - alpha * dJwithReg(theta)  
+    cat("\n")
   }
   return(theta)
 }
@@ -91,16 +115,18 @@ GradDesc <- function(theta){
   return(theta)
 }
 #We define an initial theta as a very small randomized value
-initial_theta <- rep(runif(1,0,1)*0.001,ncol(X1))
-GradDesc(initial_theta)
+#initial_theta <- rep(runif(1,0,1)*0.001,ncol(X1))
+initial_theta <-runif(ncol(X1), min = -1, max = 1)
+#GradDesc(initial_theta)
+theta_par <-grad.descent(initial_theta)
 #Cost at inital theta
 cost_theta <- JwithReg(initial_theta)
 cat("Cost theta: ", cost_theta)
 
 # We derive the theta using gradient descent with the built-in optim function from R
-theta_optim <- optim(par=initial_theta,fn=JwithReg)
+#theta_optim <- optim(par=initial_theta,fn=JwithReg)
 #Lets set the theta
-theta_par <- theta_optim$par
+#theta_par <- theta_optim$par
 
 #Cost at optimal value of the theta
 cost_theta_optim <- theta_optim$value
@@ -111,8 +137,9 @@ delta_cost <- cost_theta_optim - cost_theta;
 cat("Delta Cost: ", delta_cost)
 ### TEST ON some data
 
-abs(sign(theta_par%*%c(1,xzero[10,])-0.05)-1)/2#strange magic to set 0-> zero class 1->one class
-abs(sign(theta_par%*%c(1,xone[10,])-0.05)-1)/2
+abs(sign(theta_par%*%c(1,xzero[2,])-0.05)-1)/2#strange magic to set 0-> zero class 1->one class
+
+abs(sign(theta_par%*%c(1,xtwo[10,])-0.05)-1)/2
 
 ### Prepare test data
 #normalize
@@ -121,19 +148,37 @@ Z <- as.matrix(testDataNormalized)
 #test only on zeros and ones
 zzero <- Z[testLabels==0,]
 zone  <-Z[testLabels==1,]
+ztwo  <-Z[testLabels==2,]
+zthree  <-Z[testLabels==3,]
+zfour  <-Z[testLabels==4,]
+zfive  <-Z[testLabels==5,]
+zsix  <-Z[testLabels==6,]
+zseven  <-Z[testLabels==7,]
+zeight  <-Z[testLabels==8,]
+znine  <-Z[testLabels==9,]
+
 testLabelsZero <- testLabels[testLabels==0]
 testLabelsOne <- testLabels[testLabels==1]
+testLabelsTwo <- testLabels[testLabels==2]
+testLabelsThree <- testLabels[testLabels==3]
+testLabelsFour <- testLabels[testLabels==4]
+testLabelsFive <- testLabels[testLabels==5]
+testLabelsSix <- testLabels[testLabels==6]
+testLabelsSeven <- testLabels[testLabels==7]
+testLabelsEight <- testLabels[testLabels==8]
+testLabelsNine <- testLabels[testLabels==9]
 #Concatenate to feed classifier
-zBinary <- rbind(zzero, zone)
-testLabelsBinary <- c(testLabelsZero,testLabelsOne)
+zBinary <- rbind(zzero, znine)
+testLabelsBinary <- c(testLabelsZero,testLabelsNine)
 #Add ones
 Z1 <- cbind(rep(1,nrow(zBinary)),zBinary)
 
-predictedLabels <- abs(sign(Z1%*%theta_par-0.05)-1)/2
+predictedLabels <- abs(sign(Z1%*%theta_par)-1)/2
 
 #We define a function to evaluate the accuracy of our classifier
 binary_classifier_accuracy <- function(theta, X,y){
-  correct <- sum(y == abs(sign(X%*%theta-0.05)-1)/2)
+  correct <- sum(y == abs(sign(X%*%theta))/2)
+  
   accuracy <- correct / length(y)
   return(accuracy)
 }
@@ -165,3 +210,4 @@ cat("Recall: ", recall)
 cat("Specificity: ", specificity)
 cat("Fmeasure: ", fmeasure)
 cat("FDR: ", FDR)
+
